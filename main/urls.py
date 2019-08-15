e478274f8ab81fc65e2973593ce36ac8141b0a63
Fangyi Zhao <zoefyz12@gmail.com>
2019-08-15 12:31:14 -0700

    add messages and includes

--- main/urls.py
+++ main/urls.py
@@ -20,6 +20,6 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path("", views.homepage, name="homgpage"),
+    path("", views.homepage, name="homepage"),
     path("register/", views.register, name="register"),
 ]
--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Tutorial
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import logout, authenticate, login
+from django.contrib import messages
 
 # Create your views here.
 def homepage(request):
@@ -16,16 +17,19 @@ def register(request):
         if form.is_valid():
             user = form.save()
             username = form.cleaned_data.get('username')
+            messages.success(request, f"New account created: {username}")
             login(request, user)
             return redirect("main:homepage")
+
         else:
             for msg in form.error_messages:
-                print(form.error_messages[msg])
+                messages.error(request, f"{msg}: {form.error_messages[msg]}")
 
-            return render(request=request,
-                          template_name="main/register.html",
-                          context={"form": form})
+            return render(request = request,
+                          template_name = "main/register.html",
+                          context={"form":form})
+
     form = UserCreationForm
-    return render(request=request,
-                  template_name="main/register.html",
-                  context={"form": form})
+    return render(request = request,
+                  template_name = "main/register.html",
+                  context={"form":form})
